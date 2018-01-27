9a271fd7239639c50ccfa90939a3fd57a007bff0
Trisha Garrett <trisha.garrett@gmail.com>
2018-01-27 01:19:02 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,5 +12,12 @@ class CashRegister
     self.total += price * quantity
   end
 
+  def apply_discount
+    d = (self.total * @discount)/100
+    cash_register_with_discount = self.total - d
+      "After the discount, the total comes to $#{cash_register_with_discount}."
+  #binding.pry
+  end
+
 
 end
